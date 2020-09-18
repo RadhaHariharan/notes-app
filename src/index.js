7d73f07de06fd52903b8e415bc27e1ef225ec44a
Krishna Shukla <krish@techlify.com>
2020-09-18 12:54:06 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./components/App"
+import App from "./components/App";
 
 ReactDom.render(<App />, document.getElementById("root"));
+
+// "scripts": {
+//     "predeploy": "npm run build",
+//     "deploy": "gh-pages -d build",
+//   },
+// "homepage": "https://radhahariharan.github.io/notes-app/",
