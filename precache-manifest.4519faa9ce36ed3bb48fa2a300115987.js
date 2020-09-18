ead259a8eb5d61d701bf7b43dea55a904f3bd25c
Krishna Shukla <krish@techlify.com>
2020-09-18 12:45:30 +0530

    Updates

---
+++ precache-manifest.4519faa9ce36ed3bb48fa2a300115987.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836120d11ab43ae62d39d057c607ef3c",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "7f33b6e9cd8d41a56787",
+    "url": "/notes-app/static/js/2.8a39e989.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/notes-app/static/js/2.8a39e989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5baa9eefc95356584a5",
+    "url": "/notes-app/static/js/main.80fd6640.chunk.js"
+  },
+  {
+    "revision": "760c688f173c6bfa1fac",
+    "url": "/notes-app/static/js/runtime-main.482a3d78.js"
+  }
+]);
